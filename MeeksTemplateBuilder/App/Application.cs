8b12083ef6b2847bf7802b41dd25ea4cbf67cb29
mi222eh <mi222eh@users.noreply.github.com>
2019-03-10 20:24:27 +0100

    Created an example application that is using it

---
+++ MeeksTemplateBuilder/App/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    class Application
+    {
+        public void run()
+        {
+            TemplateGenerator templateGenerator = new TemplateGenerator();
+            string result = templateGenerator.GeneratePresentationText();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MeeksTemplateBuilder/App/TemplateGenerator.cs
@@ -0,0 +1,31 @@
+﻿using MeeksTemplateBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    class TemplateGenerator  : IMeeksTemplateProvider
+    {
+        public string GetTemplate(string name)
+        {
+            return Templates.GetTemplate(name);
+        }
+
+        public string GeneratePresentationText()
+        {
+            MeeksTemplater templater = new MeeksTemplater();
+            templater.AddTemplateProvider(this);
+            return templater.render(Templates.Presentation, new
+            {
+                TOOL_NAME = "Meeks template builder",
+                COMPANY_NAME = "Marvelous Cookie Factory",
+                START_DATE = "1970",
+                ACTIVE_TEXT = "Not active",
+                PRODUCT = "Cookies",
+                EXISTS_TEXT = "No"
+            });
+        }
+    }
+}
